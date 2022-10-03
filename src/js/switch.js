937277b722fb80ad5c305126a5c6acec33b26035
David <bigway8@vk.com>
2022-10-04 00:42:44 +0300

    lesson 1 rework

--- src/js/switch.js
+++ src/js/switch.js
@@ -1,8 +1,6 @@
 import { datecalc, timerEl } from './consts.js'
-import { timer } from './timer.js'
 
 export function switcher() {
   datecalc.classList.toggle('hide')
   timerEl.classList.toggle('hide')
-  timer()
 }
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,11 +1,33 @@
-import { timerEl } from './consts.js'
+import {
+  timerCountEl as count,
+  start,
+  pause,
+  set,
+  timerCountResultEl as result,
+} from './consts.js'
 
 let isPause = false
 setInterval(() => {
   if (isPause) {
-    timerEl.innerHTML = Number(timerEl.innerHTML) + 1
+    result.innerHTML = Number(result.innerHTML) - 1
+  }
+  if (result.innerHTML <= 0) {
+    isPause = false
   }
 }, 1000)
-export const timer = () => {
-  isPause = !isPause
+
+const handlerStart = () => {
+  if (result.innerHTML <= 0) {
+    isPause = false
+  } else {
+    isPause = true
+  }
 }
+const handlerPause = () => (isPause = false)
+export const handlerSet = () => {
+  result.innerHTML = count.value
+}
+
+export const timer = () => {}
+start.addEventListener('click', handlerStart)
+pause.addEventListener('click', handlerPause)
